6591bd0e40ab15fc0be7312da45bae3c612f89b1
Vinícius Mesquita <62081728+vimeskita@users.noreply.github.com>
2021-11-09 10:56:35 -0300

    Add files via upload

---
+++ First code/src/main/java/NewClass.java
@@ -0,0 +1,12 @@
+
+import java.util.Date;
+
+public class NewClass {
+    public static void main(String args[]){
+         System.out.println("Hello World!");
+         Date relogio = new Date();
+         System.out.println("A hora do sistema é: ");
+         System.out.println(relogio.toString());
+    }
+    
+}
